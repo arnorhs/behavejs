0aa62ab3a9adfde9b1ffdb07a5df8b80cb8ba13a
arnorhs <arnorhs@gmail.com>
2011-10-03 23:36:33 -0700

    added comments to example 02 and added a todo to the readme file

--- README.md
+++ README.md
@@ -3,7 +3,14 @@ behave as I say, UI element
 
 Behave.js is basically a statemachine for handling UI element states.
 
-
+Todo:
+  - Probably should make the stateOn and stateOff functions be executed in the
+    same context as the events and other callbacks in that the "this" value
+    should be the element's object - although that would mean we could not
+    change to elements that reference the same actual underlying items (like
+    when the same button with the same action is placed on multiple locations,
+    but that's probably also an issue with the currently executed
+    callbacks...)
 
 
 
--- examples/02-multiple-buttons/example.js
+++ examples/02-multiple-buttons/example.js
@@ -1,47 +1,60 @@
 
 /*
-   This is basically the same example as 01, but this one has
-   many buttons
+    This is basically the same example as 01, but this one has
+    many buttons and a bit more complex functionality where it
+    also has a "working" state for when it's changing it's
+    value from one to the other
+
+    Note: just to reiterate the point - this is not the
+    actual button definition.. this is just what decides
+    how a button of this type works - you can think of it
+    as the button's "class"
 */
 button = new Behave({
-    rootClass: "toggle_button",
     debug: true,
+    // setting the initial state with a callback...
     initialState: function () {
         return $(this).hasClass('on') ? 'on' : 'off';
     }
 });
 
-button.addState('off',{
-    classes: ["off"],
-    stateOn: function ($element) {
-        $element.html("Off");
-    }
-});
-
-button.addState('on',{
-    classes: ["on"],
-    stateOn: function ($element) {
-        $element.html("I'm OANN!!");
-    }
-});
-
-button.addState('working',{
-    classes: ["working"],
-    disableEvents: ["click"],
-    stateOn: function ($element) {
-        // basically do any html rendering required.. for the state
-        // eg. maybe set the disabled state on the button
-        $element.html('working...');
-        $element.attr("disabled",true);
+// defined in a shorthand object notation...
+button.addStates({
+    off: {
+        classes: ["off"],
+        stateOn: function ($element) {
+            $element.html("Off");
+        }
     },
-    stateOff: function ($element) {
-        // maybe remove the attribute for disabled...
-        $element.removeAttr("disabled");
+    on: {
+        classes: ["on"],
+        stateOn: function ($element) {
+            $element.html("I'm OANN!!");
+        }
+    },
+    // when the button is in the "working" state - then you can't
+    // click it and it's diabled.. actually setting the disabled
+    // attribute should be enough to disable it, but this is just
+    // to explain the behavior
+    working: {
+        classes: ["working"],
+        disableEvents: ["click"],
+        stateOn: function ($element) {
+            // basically do any html rendering required.. for the state
+            // eg. maybe set the disabled state on the button
+            $element.html('working...').attr("disabled",true);
+        },
+        stateOff: function ($element) {
+            // maybe remove the attribute for disabled...
+            $element.removeAttr("disabled");
+        }
     }
 });
 
+// when a button of this type is clicked the state will
+// be set to a working state and it's value then set to
+// that of the opposite to what it currently is
 button.addEvent("click", function () {
-
     var st = ($(this).state()) === "off" ? "on" : "off",
         $element = $(this);
     $element.state('working');
@@ -54,7 +67,6 @@ button.addEvent("click", function () {
 $(function(){
 
     button.addCollection($('.row button'));
-
 
 });
 
